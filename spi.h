9861809f16ca5c303ac8668928be2485bcc3439d
DafneDC <78834522+DafneDC@users.noreply.github.com>
2021-04-20 19:38:51 -0500

    Add files via upload

---
+++ spi.h
@@ -0,0 +1,12 @@
+#include "stdbool.h"
+#include "sam.h"
+
+#ifndef SPI_H_
+#define SPI_H_
+
+#define BAUDRATE 9600
+
+void spiInit( void );
+uint8_t spiSend( uint8_t data );
+
+#endif /* SPI_H_ */
---
+++ uart.h
@@ -0,0 +1,14 @@
+/*
+ * IncFile1.h
+ *
+ * Created: 4/13/2021 5:30:50 PM
+ *  Author: L03068681
+ */ 
+
+
+#ifndef INCFILE1_H_
+#define INCFILE1_H_
+
+void UARTInit(void);
+
+#endif /* INCFILE1_H_ */
